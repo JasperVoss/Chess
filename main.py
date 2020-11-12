34c8605422140d2b9df48dceb5d961253ee355ca
Jasper Voss <jaspervoss812@gmail.com>
2020-11-12 17:53:52 -0500

    upload

--- main.py
+++ main.py
@@ -13,3 +13,22 @@ def magnet_off():
 
 magnet_off()
 
+r = move.get_radii([300, 200])
+
+for i in range(5):
+	magnet_on()
+	r = move.move(r, [300, 300])
+	magnet_off()
+	r = move.move(r, [250, 250])
+	magnet_on()
+	r = move.move(r, [350, 250])
+	magnet_off()
+	r = move.move(r, [300, 300])
+	magnet_on()
+	r = move.move(r, [300, 200])
+	magnet_off()
+	r = move.move(r, [350, 250])
+	magnet_on()
+	r = move.move(r, [250, 250])
+	magnet_off()
+	r = move.move(r, [300, 200])
