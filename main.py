03b153a1a30b82c957b59615dfedf42142d2eaf4
Jasper Voss <jaspervoss812@gmail.com>
2020-11-14 17:50:03 -0500

    upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as gpio
 import move, time
+from random import randint
 magnet_pin = 4
 
 gpio.setmode(gpio.BCM)
@@ -16,11 +17,11 @@ magnet_off()
 while True:
 	move.move([100, 100])
 	time.sleep(.5)
-	move.move([500, 100])
+	move.move([randint(100, 500), randint(100, 400)])
+	time.sleep(.5)
+	move.move([randint(100, 500), randint(100, 400)])
 	time.sleep(.5)
 	move.move([500, 400])
-	time.sleep(.5)
-	move.move([100, 400])
 	time.sleep(.5)
 
 # r = move.get_radii([400, 300])
