de4d582dfcf972662393bcd5f5aae0e35409411d
Jasper Voss <jaspervoss812@gmail.com>
2020-11-17 15:00:12 -0500

    upload

--- move.py
+++ move.py
@@ -1,9 +1,9 @@
 import math, time, motors
 
 mm_per_step_0 = .265
-mm_per_step_1 = .2645
-mm_per_step_2 = .265
-mm_per_step_3 = .2655
+mm_per_step_1 = .266
+mm_per_step_2 = .2673
+mm_per_step_3 = .266
 
 def get_curr_step():
     file = open("curr_steps.txt", "r")
