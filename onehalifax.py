963b08eea6d12a214554aceed7d663b398348b48
Jasper Voss <jaspervoss812@gmail.com>
2020-11-23 11:24:19 -0500

    halifax

--- onehalifax.py
+++ onehalifax.py
@@ -7,14 +7,14 @@ inPins = [26, 19, 13, 6, 5, 11, 9, 10, 22, 23]
 
 gpio.setmode(gpio.BCM)
 for i in outPins:
-    gpio.setup(i, gpio.IN)
+    gpio.setup(i, gpio.OUT)
+    gpio.output(i, 0)
 for i in inPins:
     gpio.setup(i, gpio.IN)
 
-gpio.setup(outPins[0], gpio.OUT)
+gpio.output(outPins[0], 1)
 
 while True:
-    gpio.output(outPins[0], 1)
     if gpio.input(inPins[0]) == gpio.LOW:
         status = 1
     else:
