3a228f7b66056b6d240128c7c93be44478b967ac
Jasper Voss <jaspervoss812@gmail.com>
2020-12-25 10:58:50 -0500

    adf

--- bounceback.py
+++ bounceback.py
@@ -20,9 +20,10 @@ while True:
 	state = halifax.get_state()
 	for i in range(len(state)):
 		for j in range(len(state[i])):
-			if state[i][j] == 1 and i != squarey and j != squarex:
-				move.move_square(i, j)
-				magnet_on()
-				move.move_piece(squarey, squarex)
-				magnet_off()
+			if state[i][j] == 1:
+				if i != squarey or j != squarex:
+					move.move_square(i, j)
+					magnet_on()
+					move.move_piece(squarey, squarex)
+					magnet_off()
 
